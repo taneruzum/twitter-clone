4d9768b2c174fe442853621cb2a13ba0075aa060
Taner ÜZÜM <uzumtaner.official@gmail.com>
2023-09-21 17:33:54 +0300

    Added profile section.

--- src/layouts/main/sidebar/index.jsx
+++ src/layouts/main/sidebar/index.jsx
@@ -1,11 +1,17 @@
 import Logo from "./logo";
 import Menu from "./menu";
+import Profile from "./menu/profile";
 
-export default function Sidebar(){
-    return(
-        <aside className="w-[275px] min-h-screen px-2">
-            <Logo/>
-            <Menu/>
+export default function Sidebar() {
+    return (
+        <aside className="w-[275px] min-h-screen px-2 py-3 flex flex-col items-start justify-between ">
+            <div className="w-full">
+                <Logo />
+                <Menu />
+            </div>
+
+            <Profile />
         </aside>
+
     )
 }
--- src/layouts/main/sidebar/menu/index.jsx
+++ src/layouts/main/sidebar/menu/index.jsx
@@ -5,6 +5,7 @@ import More from "./more";
 import New from "./new";
 import { useState, useEffect } from "react";
 
+
 export default function Menu() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
@@ -24,46 +25,50 @@ export default function Menu() {
   console.log(windowWidth);
 
   return (
-    <nav className="mt-0.5 mb-1">
-      {mainMenu.map((menu, index) => {
-        if ( // daha iyi bir yöntem bulana kadar en iyisi bu
-          menu.title === "Yer İşaretleri" &&
-          // 1280 < windowWidth &&
-          windowWidth < 1540
-        ) {
-          return null;
-        }
-        return (
-          <NavLink to={menu.path} className="py-[.2px] block group" key={index}>
-            {({ isActive }) => (
-              <div
-                className={classNames(
-                  "p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]",
-                  {
-                    "font-bold": isActive,
-                  }
-                )}
-              >
-                <div className="w-[26.25px] h-[26.25px] relative">
-                  {menu?.notification && (
-                    <span className="w-[18px] h-[18px] rounded-full bg-[#1d9bf0] absolute -top-1.5 -right-1 flex justify-center items-center text-[11px]">
-                      {menu?.notification}
-                    </span>
+    <nav className="mt-0.5 mb-1 ">
+      
+        {mainMenu.map((menu, index) => {
+          if ( // daha iyi bir yöntem bulana kadar en iyisi bu
+            menu.title === "Yer İşaretleri" &&
+            // 1280 < windowWidth &&
+            windowWidth < 1540
+          ) {
+            return null;
+          }
+          return (
+            <NavLink to={menu.path} className="py-[.2px] block group" key={index}>
+              {({ isActive }) => (
+                <div
+                  className={classNames(
+                    "p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]",
+                    {
+                      "font-bold": isActive,
+                    }
                   )}
-                  {!isActive && menu.icon.passive}
-                  {isActive && menu.icon.active}
+                >
+                  <div className="w-[26.25px] h-[26.25px] relative">
+                    {menu?.notification && (
+                      <span className="w-[18px] h-[18px] rounded-full bg-[#1d9bf0] absolute -top-1.5 -right-1 flex justify-center items-center text-[11px]">
+                        {menu?.notification}
+                      </span>
+                    )}
+                    {!isActive && menu.icon.passive}
+                    {isActive && menu.icon.active}
+                  </div>
+                  <div className={"text-xl pr-4"}>
+                    {menu.title}
+                  </div>
                 </div>
-                <div className={classNames("text-xl pr-4", {})}>
-                  {menu.title}
-                </div>
-              </div>
-            )}
-          </NavLink>
-        );
-      })}
+              )}
+            </NavLink>
+          );
+        })}
+
+        
 
       <More />
       <New />
+      
     </nav>
   );
 }

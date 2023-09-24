53b99bf139ea09bc5b4b915d8973a383823ab1e3
Taner ÜZÜM <uzumtaner.official@gmail.com>
2023-09-24 20:24:54 +0300

    Popover has designed

--- src/layouts/main/sidebar/menu/account/index.jsx
+++ src/layouts/main/sidebar/menu/account/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import { Popover } from '@headlessui/react'
+import { Popover, Transition } from '@headlessui/react'
 
-import { useAccount } from '~/store/auth/hook';
+import { useAccount } from '~/store/auth/hooks';
 import MoreAccount from './moreAccount';
 
 
+
 function Account() {
+
     const account = useAccount()
-    
+
     return (
         <Popover className="relative w-full">
-            <Popover.Button className={'flex  my-1 justify-between items-center w-full p-3 rounded-full  transition-all hover:bg-[#eff3f41a] outline-none'}>
+            <Popover.Button className={"flex  my-1 justify-between items-center w-full p-3 rounded-full  transition-all hover:bg-[#eff3f41a] outline-none"}>
+
                 <div className='flex gap-x-2 items-center'>
 
-                    <img className='w-10 h-10 rounded-full ' src={account.avatar} alt=""  />
+                    <img className='w-10 h-10 rounded-full ' src={account.avatar} alt="" />
 
                     <div className='flex flex-col '>
                         <span className='text-white font-bold text-left '>{account.userName}</span>
@@ -26,12 +29,23 @@ function Account() {
                 <svg viewBox="0 0 24 24" width={18.75} height={18.75}><g><path fill='#fff' d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path></g></svg>
 
             </Popover.Button>
+            <Transition
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0"
+            >
+                <Popover.Panel className="w-[300px] absolute left-1/2 -translate-x-1/2 bottom-[80px] p-2.5 bg-black shadow-box rounded-xl overflow-hidden flex flex-col justify-between items-start ">
+                    <MoreAccount />
 
-            <Popover.Panel className="w-[300px] absolute left-1/2 -translate-x-1/2 bottom-full py-3 bg-black shadow-box rounded-xl overflow-hidden flex flex-col justify-between items-start">
-                <MoreAccount/>
-                
 
-            </Popover.Panel>
+                </Popover.Panel>
+
+
+            </Transition>
+
         </Popover>
 
     )
--- src/layouts/main/sidebar/menu/account/moreAccount/index.jsx
+++ src/layouts/main/sidebar/menu/account/moreAccount/index.jsx
@@ -1,18 +1,22 @@
 
 import Line from '~/components/line';
-import { useAccount, useAccounts } from '~/store/auth/hook';
+import { useAccount, useAccounts } from '~/store/auth/hooks';
+import classNames from 'classnames';
+
 export default function MoreAccount() {
-    const account = useAccount()
+    const currentAccount = useAccount()
     const accounts = useAccounts()
-    
+
     console.log(accounts.length);
     return (
         <>
             {accounts.map((account, id) =>
             (
 
-                <button key={id} className={'flex  my-1 justify-between items-center w-full p-3 transition-all cursor-pointer hover:bg-[#eff3f41a] outline-none'}>
-                    <div className='flex gap-x-2 items-center'>
+                <button key={id} className={classNames("flex  my-1 justify-between items-center w-full p-3 pr-5 transition-all  outline-none cursor-auto ", {
+                    "hover:bg-[#eff3f41a] cursor-pointer cursor- ": currentAccount.id !== account.id
+                })}>
+                    <div className='flex gap-x-2 items-center w-full'>
 
                         <img className='w-10 h-10 rounded-full ' src={account?.avatar} alt="" />
 
@@ -21,7 +25,15 @@ export default function MoreAccount() {
                             <span className='text-[#71767B] text-left'> {account?.fullName}</span>
                         </div>
 
+                        {currentAccount?.id === account?.id && (
+                            <svg viewBox="0 0 24 24" className="ml-auto" width={18.75} height={18.75} fill="#00ba7c">
+                                <path
+                                    d="M12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-.81 14.68l-4.1-3.27 1.25-1.57 2.47 1.98 3.97-5.47 1.62 1.18-5.21 7.15z"
+                                />
+                            </svg>
+                        )}
                     </div>
+
 
                 </button>
 
@@ -33,12 +45,12 @@ export default function MoreAccount() {
             <button className="text-left px-3  py-2.5 font-bold text-white w-full  transition-all cursor-pointer  hover:bg-[#eff3f41a] ">
                 Var olan bir hesap ekle
             </button>
-            {accounts?.length >1 &&  (<button className="text-left px-3  py-2.5 font-bold text-white w-full  transition-all cursor-pointer  hover:bg-[#eff3f41a] ">Hesapları Yönet</button>)}
+            {accounts?.length > 1 && (<button className="text-left px-3  py-2.5 font-bold text-white w-full  transition-all cursor-pointer  hover:bg-[#eff3f41a] ">Hesapları Yönet</button>)}
 
             <button className="text-left px-3 py-2.5 font-bold text-white  w-full transition-all cursor-pointer hover:bg-[#eff3f41a]">
-                {account.fullName} hesabından <br /> çıkış yap
+                {currentAccount.fullName} hesabından <br /> çıkış yap
             </button>
         </>
     )
-    
+
 }

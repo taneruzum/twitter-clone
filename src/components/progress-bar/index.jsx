a4e6dcf07de5f2ed03abc3ba2a9f9dcfc37ff799
Uğur Akçay <akcayugur01@gmail.com>
2023-10-07 15:26:48 +0300

    commit-test

---
+++ src/components/progress-bar/index.jsx
@@ -0,0 +1,9 @@
+
+
+function ProgressBar() {
+  return (
+    <div></div>
+  )
+}
+
+export default ProgressBar
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,5 +19,6 @@ export default {
     variants: {},
     plugins: [],
   },
-  plugins: [],
+  
+  plugins: [require("daisyui")],
 };

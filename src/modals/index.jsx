eca0664bdc4a7f9d42eb079cb8eff0a708cec9bc
Taner ÜZÜM <uzumtaner.official@gmail.com>
2023-10-08 16:27:27 +0300

    Fixed layout bugs.

--- src/modals/index.jsx
+++ src/modals/index.jsx
@@ -8,7 +8,7 @@ export default function Modal() {
 	const currentModal = modals.find(m => m.name === modal.name)
 
 	return (
-		<div className="fixed inset-0 bg-[color:var(--background-modal)] flex items-center justify-center z-20">
+		<div className="fixed inset-0 bg-[color:var(--background-modal)] flex items-center justify-center z-30">
 			<div className="bg-[color:var(--background-primary)] max-w-[600px] max-h-[90vh] overflow-x-hidden rounded-2xl">
 				{currentModal && <currentModal.element close={removeModal} />}
 			</div>
